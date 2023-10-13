fa1fe58739f426d86964864233614a2f94b08d39
alanlopez1 <alanlopez@ciencias.unam.mx>
2023-10-13 02:54:55 -0600

    Added some functions for future functionality

--- practica2/src/certificate_gen.py
+++ practica2/src/certificate_gen.py
@@ -1,12 +1,13 @@
 import random
 
+
 def build_ady(ady: list[str]) -> dict[int, list[int]]:
     graph = {}
 
     for idx, a in enumerate(ady[:-1], start=1):
         graph[idx] = []
         for ele, i in enumerate(range(0, len(a) + 2, 2)):
-            match a[i - 2 : i]:
+            match a[i - 2: i]:
                 case "00":
                     continue
                 case "11":
@@ -14,9 +15,11 @@ def build_ady(ady: list[str]) -> dict[int, list[int]]:
 
     return graph
 
+
 def build_k(ady: list[str]) -> int:
     k = ady[-1]
     return int(k, 2)
+
 
 def decoder(in_str: str) -> tuple[dict[int, list[int]], int]:
     parsing_line = True
@@ -25,7 +28,7 @@ def decoder(in_str: str) -> tuple[dict[int, list[int]], int]:
 
     for i in range(0, len(in_str), 2):
         c = in_str[i-2:i]
-        #print(c)
+        # print(c)
         if c == "10":
             tmp = in_str[i:]
             x.append(tmp)
@@ -40,10 +43,11 @@ def decoder(in_str: str) -> tuple[dict[int, list[int]], int]:
         tmp_line += c
 
     graph = build_ady(x)
-    k     = build_k(x)
+    k = build_k(x)
     return graph, k
 
-def certificate(k: int, graph) -> list[int]:
+
+def certificate(k: int, graph: dict[int, list[int]]) -> list[int]:
     perm = []
     count = 0
     size = len(graph.keys()) + 1
@@ -59,10 +63,87 @@ def certificate(k: int, graph) -> list[int]:
 
     return perm
 
-t = "0000001111110100001111111101000000111111011111110011000111111100000001111111000000011011"
-d, k = decoder(t)
-print(d, k)
-print()
 
-p = certificate(k, d)
-print(f"Certificate of length {k}: {p}")
+# t = "0000001111110100001111111101000000111111011111110011000111111100000001111111000000011011"
+# d, k = decoder(t)
+# print(d, k)
+# print()
+
+# p = certificate(k, d)
+# print(f"Certificate of length {k}: {p}")
+
+
+def run():
+    """
+    Funcion que se encarga de gestionar archivos de entrada, salida y correr el programa.
+    """
+
+    # entrada = input("Escribe nombre de archivo de entrada: ")
+    entrada = "entrada.txt"
+    r = open(entrada, "r").read()
+    # ? informacion: Número de vértices, Número de aristas, k
+
+    c = certificado(r)
+
+    # salida = input("Escribe nombre de archivo de salida: ")
+    salida = "salida.txt"
+    w = open(salida, "w")
+    w.write(str(c))
+
+
+def certificado(s: str):
+    """
+    Recibe del archivo num_nodos, num_aristas, k.
+    Crea grafo aleatorio y genera certificado.
+    Valida certificado.
+    """
+    n, e, k = map(int, s.split())
+    rand_graph = random_graph(n, e)
+    print(rand_graph)
+    cert = certificate(k, rand_graph)
+    print(cert)
+
+    valido = validate(cert, rand_graph)
+    print(valido)
+
+
+def validate(cert: list[int], graph: dict[int, list[int]]) -> bool:
+    """
+    Función explicada por Malinali, valida que el certificado sea válido.
+
+    Obteniendo una ruta al azar, checa que las aristas se encuentren en la gráfica.
+    """
+    for i in range(len(cert)-1):
+        u = cert[i]
+        v = cert[i+1]
+        if v not in graph[u]:
+            return False
+
+    # Checa ciclo
+    if cert[0] in graph[cert[-1]]:
+        return False
+
+    return True
+
+
+def random_graph(n: int, e: int) -> dict[int, list[int]]:
+    graph = {i: [] for i in range(1, n+1)}
+    edges = set()
+
+    while e:
+        u = random.randint(1, n)
+        v = random.randint(1, n)
+        edge = (min(u, v), max(u, v))
+        if u == v or edge in edges:
+            continue
+
+        edges.add(edge)
+        graph[u].append(v)
+        graph[v].append(u)
+
+        e -= 1
+
+    return graph
+
+
+run()
